a94808aaded15196f3f6636b9db4d047420cc25c
wanghao <117663444@qq.com>
2019-07-31 12:09:17 +0800

    Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostComponent } from './modules/post/post.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: 'posts', component: PostComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
