8d9619f66408319d5806abf0b7f696134c75229d
wanghao <117663444@qq.com>
2019-07-31 12:32:12 +0800

    Define route with params

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PostComponent } from './modules/post/post.component';
+import { PostDetailsComponent } from './modules/post/components/post-details/post-details.component';
 
-const routes: Routes = [{ path: 'posts', component: PostComponent }];
+const routes: Routes = [
+  { path: 'posts', component: PostComponent },
+  {
+    path: 'posts/:id',
+    component: PostDetailsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/post/post.module.ts
+++ src/app/modules/post/post.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostComponent } from './post.component';
 import { PostItemComponent } from './components/post-item/post-item.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
 
 @NgModule({
-  declarations: [PostComponent, PostItemComponent],
+  declarations: [PostComponent, PostItemComponent, PostDetailsComponent],
   imports: [CommonModule],
   exports: [PostComponent],
 })
