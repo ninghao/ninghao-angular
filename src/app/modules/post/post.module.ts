6499bfb5fc07313e61302c499e0890dde7b3d43f
wanghao <117663444@qq.com>
2019-07-31 12:41:23 +0800

    routerLink

--- src/app/modules/post/post.module.ts
+++ src/app/modules/post/post.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { PostComponent } from './post.component';
 import { PostItemComponent } from './components/post-item/post-item.component';
 import { PostDetailsComponent } from './components/post-details/post-details.component';
+import { AppRoutingModule } from '../../app-routing.module';
 
 @NgModule({
   declarations: [PostComponent, PostItemComponent, PostDetailsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, AppRoutingModule],
   exports: [PostComponent],
 })
 export class PostModule {}
