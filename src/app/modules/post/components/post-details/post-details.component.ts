a6037552f2e0066f751506b10f29bc287506d182
wanghao <117663444@qq.com>
2019-07-31 14:23:13 +0800

    paramMap

--- src/app/modules/post/components/post-details/post-details.component.ts
+++ src/app/modules/post/components/post-details/post-details.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post-details',
   templateUrl: './post-details.component.html',
-  styleUrls: ['./post-details.component.css']
+  styleUrls: ['./post-details.component.css'],
 })
 export class PostDetailsComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const postId = +params.get('id');
+      console.log(postId);
+    });
   }
-
 }
