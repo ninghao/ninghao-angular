4225d4077dcac3a0bdcb16ad5c7b691c43ae6992
wanghao <117663444@qq.com>
2019-08-01 17:56:04 +0800

    User module with register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PostComponent } from './modules/post/post.component';
 import { PostDetailsComponent } from './modules/post/components/post-details/post-details.component';
+import { RegisterComponent } from './modules/user/components/register/register.component';
 
 const routes: Routes = [
   { path: 'posts', component: PostComponent },
   {
     path: 'posts/:id',
     component: PostDetailsComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostModule } from './modules/post/post.module';
+import { UserModule } from './modules/user/user.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PostModule],
+  imports: [BrowserModule, AppRoutingModule, PostModule, UserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
