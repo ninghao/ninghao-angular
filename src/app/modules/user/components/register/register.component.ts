6ceed7f5aaa3f8934e133edcee5b9ba479a70497
wanghao <117663444@qq.com>
2019-08-02 09:23:47 +0800

    FormBuilder

--- src/app/modules/user/components/register/register.component.ts
+++ src/app/modules/user/components/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,15 +12,20 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  registerForm = new FormGroup({
-    username: new FormControl('', Validators.required),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(6),
-    ]),
+  // registerForm = new FormGroup({
+  //   username: new FormControl('', Validators.required),
+  //   password: new FormControl('', [
+  //     Validators.required,
+  //     Validators.minLength(6),
+  //   ]),
+  // });
+
+  registerForm = this.formBuilder.group({
+    username: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength]],
   });
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.registerForm.valueChanges.subscribe(value => {
