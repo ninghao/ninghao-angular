846ffdd53eab10f431100f273106a959377bd3cf
wanghao <117663444@qq.com>
2019-08-05 21:44:49 +0800

    Admin component

--- src/app/modules/demo/demo-routing.module.ts
+++ src/app/modules/demo/demo-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { DemoComponent } from './demo.component';
 import { ChildRoutesComponent } from './child-routes/child-routes.component';
 import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
       {
         path: 'login',
         component: LoginComponent,
+      },
+      {
+        path: 'admin',
+        component: AdminComponent,
       },
     ],
   },
--- src/app/modules/demo/demo.module.ts
+++ src/app/modules/demo/demo.module.ts
@@ -5,10 +5,11 @@ import { DemoRoutingModule } from './demo-routing.module';
 import { DemoComponent } from './demo.component';
 import { ChildRoutesComponent } from './child-routes/child-routes.component';
 import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
 
 
 @NgModule({
-  declarations: [DemoComponent, ChildRoutesComponent, LoginComponent],
+  declarations: [DemoComponent, ChildRoutesComponent, LoginComponent, AdminComponent],
   imports: [
     CommonModule,
     DemoRoutingModule
