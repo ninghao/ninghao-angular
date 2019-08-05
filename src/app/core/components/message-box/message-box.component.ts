83beeebcce9af4d6c0d5be0c1ff5e7c2cfeaec4f
wanghao <117663444@qq.com>
2019-08-05 13:14:07 +0800

    Clearing secondary routes

--- src/app/core/components/message-box/message-box.component.ts
+++ src/app/core/components/message-box/message-box.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-message-box',
   templateUrl: './message-box.component.html',
-  styleUrls: ['./message-box.component.css']
+  styleUrls: ['./message-box.component.css'],
 })
 export class MessageBoxComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  closePopup() {
+    this.router.navigate([{ outlets: { popup: null } }]);
   }
-
 }
