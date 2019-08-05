6ad957b6711b986aea71c3d2b22d83983f29b59c
wanghao <117663444@qq.com>
2019-08-05 09:16:46 +0800

    DemoModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,17 @@ import { PostModule } from './modules/post/post.module';
 import { UserModule } from './modules/user/user.module';
 import { HeaderComponent } from './core/components/header/header.component';
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+import { DemoModule } from './modules/demo/demo.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, PageNotFoundComponent],
-  imports: [BrowserModule, PostModule, UserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    PostModule,
+    UserModule,
+    DemoModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
