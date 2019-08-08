323c1e5d8f80a2f5f2acfa97a94bc8d730e11c2b
wanghao <117663444@qq.com>
2019-08-08 17:20:06 +0800

    Observable

--- src/app/modules/demo/observable-demo/observable-demo.component.ts
+++ src/app/modules/demo/observable-demo/observable-demo.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-observable-demo',
   templateUrl: './observable-demo.component.html',
-  styleUrls: ['./observable-demo.component.css']
+  styleUrls: ['./observable-demo.component.css'],
 })
 export class ObservableDemoComponent implements OnInit {
+  demoObservable: Observable<string>;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.demoObservable = of('hello', 'hola', '您好');
   }
 
+  onClick() {
+    this.demoObservable.subscribe(value => console.log(value));
+  }
 }
