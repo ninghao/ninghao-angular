b11fc0a8dcc7973313687c8b0e240aa9fa10a489
wanghao <117663444@qq.com>
2019-08-09 16:26:53 +0800

    Clean up the post service

--- src/app/modules/post/services/post.service.ts
+++ src/app/modules/post/services/post.service.ts
@@ -1,22 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Post } from '../models/post.model';
-import { posts } from '../posts';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostService {
-  entities: Post[];
+  constructor() {}
 
-  constructor() {
-    this.entities = posts;
-  }
+  index() {}
 
-  index() {
-    return this.entities;
-  }
-
-  show(id: number) {
-    return this.entities.find(post => post.id === id);
-  }
+  show(id: number) {}
 }
