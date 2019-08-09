ec19d5692f9cf07481f2e25919379eb149d0ae15
wanghao <117663444@qq.com>
2019-08-09 16:31:38 +0800

    Setup Http Client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,13 @@ import { MessageBoxComponent } from './core/components/message-box/message-box.c
     PageNotFoundComponent,
     MessageBoxComponent,
   ],
-  imports: [BrowserModule, PostModule, UserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    PostModule,
+    UserModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/modules/post/services/post.service.ts
+++ src/app/modules/post/services/post.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   index() {}
 
